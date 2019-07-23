ce22ea9dae7a729576aaa90026f5a3a631621b49
Irawan Firmansyah <irawanfrmnsyh@gmail.com>
2019-07-23 09:35:34 +0700

    turn on private route

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -4,26 +4,26 @@ import { ACCESS_TOKEN, USER_ROLE } from '../constants';
 
 export const PrivateRoute = ({ component: Component, roles, onClick, ...rest }) => (
     <Route {...rest} render={props => {
-        // let currentUser = localStorage.getItem(ACCESS_TOKEN);
-        // let role = localStorage.getItem(USER_ROLE);
+        let currentUser = localStorage.getItem(ACCESS_TOKEN);
+        let role = localStorage.getItem(USER_ROLE);
 
-        // if (!currentUser) {
-        //     console.log('not logged in')
-        //     return <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
-        // }
+        if (!currentUser) {
+            console.log('not logged in')
+            return <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+        }
 
-        // if (roles && roles.indexOf(role) === -1
-        //     && role === 'ROLE_USER') {
-        //     return <Redirect to={{ pathname: '/' }} />
-        // }
-        // if (roles && roles.indexOf(role) === -1
-        //     && role === 'ROLE_ADMIN') {
-        //     return <Redirect to={{ pathname: '/admin' }} />
-        // }
-        // if (roles && roles.indexOf(role) === -1
-        //     && role === 'ROLE_CS') {
-        //     return <Redirect to={{ pathname: '/overview' }} />
-        // }
+        if (roles && roles.indexOf(role) === -1
+            && role === 'ROLE_USER') {
+            return <Redirect to={{ pathname: '/' }} />
+        }
+        if (roles && roles.indexOf(role) === -1
+            && role === 'ROLE_ADMIN') {
+            return <Redirect to={{ pathname: '/admin' }} />
+        }
+        if (roles && roles.indexOf(role) === -1
+            && role === 'ROLE_CS') {
+            return <Redirect to={{ pathname: '/overview' }} />
+        }
         return <Component {...props} onClick={onClick} />
     }} />
 )
